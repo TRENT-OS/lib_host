ad1836e69b98caca26d8e8126fb9d198674ccb89
Benedikt Driessen <benedikt.driessen.external@hensoldt-cyber.com>
2020-09-18 16:30:51 +0200

    [NEW] SEOS-1732: Add unit tests for HostEntropy

---
+++ test/src/Test_HostEntropy.cpp
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (C) 2019, Hensoldt Cyber GmbH
+ */
+
+#include <gtest/gtest.h>
+
+extern "C"
+{
+#include "OS_Dataport.h"
+#include "interfaces/if_OS_Entropy.h"
+#include "lib_host/HostEntropy.h"
+#include <stdint.h>
+#include <limits.h>
+}
+
+extern "C"
+{
+    extern FakeDataport_t* hostEntropy_dp;
+    static if_OS_Entropy_t entropy =
+        IF_OS_ENTROPY_ASSIGN(
+            HostEntropy,
+            hostEntropy_dp);
+}
+
+class Test_HostEntropy : public testing::Test
+{
+    protected:
+};
+
+TEST(Test_HostEntropy, read)
+{
+    size_t sz, zero;
+
+    sz  = OS_Dataport_getSize(entropy.dataport);
+    memset(OS_Dataport_getBuf(entropy.dataport), 0, sz);
+
+    // Read as much as dataport allows
+    ASSERT_EQ(sz, entropy.read(sz));
+
+    // Make a little check on the amount of zeros we find in the "random" string.
+    // We would expect sz/256 occurences (assuming a perfectly random distribution),
+    // but the reality is different. Since we do not want any test failuires, lets
+    // make sure at least we don't have too many zeroes..
+    zero = 0;
+    for (size_t i = 0; i < sz; i++)
+    {
+        zero += (0x00 == (*((uint8_t*)OS_Dataport_getBuf(entropy.dataport) + i))) ? 1 :
+                0;
+    }
+    ASSERT_LE(zero, sz >> 4);
+}
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
